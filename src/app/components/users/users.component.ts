c6378a32e4def3194e2026f5a2cd847dc304a55a
ihor-vip <vip20140902@gmail.com>
2023-03-05 14:54:13 +0200

    add all remaining files

---
+++ src/app/components/users/users.component.ts
@@ -0,0 +1,19 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {UserService} from "../../services/user.service";
+import {User} from "../../models/User";
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.css']
+})
+export class UsersComponent implements OnInit{
+  users: User[] = [];
+
+  constructor(private userService: UserService) {
+  }
+
+  ngOnInit() {
+      this.userService.getUsers().subscribe(value => this.users = value)
+    }
+}
