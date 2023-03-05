14ddce4738931161bd712948b768e97fbb88d8bb
ihor-vip <vip20140902@gmail.com>
2023-03-05 15:31:22 +0200

    add posts and comments routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,22 @@ import {HttpClientModule} from "@angular/common/http";
 import { UserComponent } from './components/user/user.component';
 import { UsersComponent } from './components/users/users.component';
 import {RouterModule, Routes} from "@angular/router";
+import { PostsComponent } from './components/posts/posts.component';
+import { CommentsComponent } from './components/comments/comments.component';
 
 const appRoute: Routes = [
-  {path: 'users', component: UsersComponent}
+  {path: 'users', component: UsersComponent},
+  {path: 'posts', component: PostsComponent},
+  {path: 'comments', component: CommentsComponent},
 ]
 
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent,
-    UsersComponent
+    UsersComponent,
+    PostsComponent,
+    CommentsComponent
   ],
   imports: [
     BrowserModule,
