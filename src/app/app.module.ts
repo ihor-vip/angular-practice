167e464130cbae5fdb75ba4d90af987773eb8f0c
ihor-vip <vip20140902@gmail.com>
2023-03-05 15:25:12 +0200

    configure routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,11 @@ import { AppComponent } from './app.component';
 import {HttpClientModule} from "@angular/common/http";
 import { UserComponent } from './components/user/user.component';
 import { UsersComponent } from './components/users/users.component';
+import {RouterModule, Routes} from "@angular/router";
+
+const appRoute: Routes = [
+  {path: 'users', component: UsersComponent}
+]
 
 @NgModule({
   declarations: [
@@ -14,7 +19,8 @@ import { UsersComponent } from './components/users/users.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(appRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
