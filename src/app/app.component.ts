db2e896015f39ca25a8d6441979b7ba22edf9d96
ihor-vip <vip20140902@gmail.com>
2023-03-05 12:59:08 +0200

    fetching data from jsonplaceholder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-practice';
+export class AppComponent implements OnInit{
+  users: any[] = [];
+
+  constructor(private http: HttpClient) {
+  }
+  ngOnInit(): void {
+    this.http.get<any[]>('https://jsonplaceholder.typicode.com/users').subscribe(value => {
+      console.log(value)
+      this.users = value
+    })
+  }
 }
