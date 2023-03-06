ef529ea19a634b5145f7f44d34615bbbb4928d0f
ihor-vip <vip20140902@gmail.com>
2023-03-06 17:27:12 +0200

    instanr redirect to posts page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import {UserResolveService} from "./services";
 import {TestGuard} from "./guards/test.guard";
 
 const appRoute: Routes = [
+  {path: '', redirectTo: 'posts', pathMatch: 'full'},
   {
     path: '', component: HomeComponent, children: [
       {
