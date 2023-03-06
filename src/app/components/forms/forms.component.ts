e543e820cb4d9ca9f0ebd25d666b5b4f73a4ce42
ihor-vip <vip20140902@gmail.com>
2023-03-06 18:25:39 +0200

    create custom validator

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {AbstractControl, FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-forms',
@@ -14,22 +14,27 @@ export class FormsComponent {
 
     myForm: FormGroup;
 
+    customValidator(control:AbstractControl):null | object {
+      // console.log(control);
+     return control.value.includes('huck') ? {achtung: 'Error'} : null
+    }
+
     ngOnInit(): void {
       this.myForm = new FormGroup({
         // name: new FormControl('', [Validators.required, Validators.minLength(7)]),
-        name: new FormControl('', [Validators.minLength(7)]),
+        name: new FormControl('', [Validators.minLength(7), this.customValidator]),
         age: new FormControl(10)
       })
     }
 
   save(tref: HTMLFormElement) {
-    console.log(tref['username'].value);
-    console.log(this.user);
+    // console.log(tref['username'].value);
+    // console.log(this.user);
   }
 
   save2() {
-    console.log(this.myForm);
-    console.log(this.myForm.controls['age'].value);
-    console.log(this.myForm.getRawValue());
+    // console.log(this.myForm);
+    // console.log(this.myForm.controls['age'].value);
+    // console.log(this.myForm.getRawValue());
   }
 }
