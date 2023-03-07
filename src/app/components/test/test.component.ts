03cc71edff06e40b6153523aaadf217532f18613
ihor-vip <vip20140902@gmail.com>
2023-03-07 14:29:24 +0200

    create custom pipe, using in ts file

--- src/app/components/test/test.component.ts
+++ src/app/components/test/test.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {IncrementPipe} from "../../pipes";
 
 @Component({
   selector: 'app-test',
   templateUrl: './test.component.html',
   styleUrls: ['./test.component.css']
 })
-export class TestComponent {
+export class TestComponent implements OnInit{
+  arr2:number[];
+
   user = {
     name: 'max',
     age: 18
@@ -15,4 +18,10 @@ export class TestComponent {
   now = new Date()
   arr = [1,2,3,4,5]
 
+  constructor(private incrementPipe: IncrementPipe) {
+  }
+
+  ngOnInit() {
+    this.arr2 = this.incrementPipe.transform(this.arr, 10)
+  }
 }
