0eb95bf42912dc1fb73bc57b727c17eb849b0273
ihor-vip <vip20140902@gmail.com>
2023-03-08 10:38:53 +0200

    display cars if user admin = authorize

--- src/app/components/admin/cars/car/car.component.ts
+++ src/app/components/admin/cars/car/car.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {ICar} from "../../../../interfaces";
 
 @Component({
   selector: 'app-car',
   templateUrl: './car.component.html',
   styleUrls: ['./car.component.css']
 })
-export class CarComponent {
+export class CarComponent implements OnInit{
+  @Input()
+  car:ICar
+
+  constructor() {
+  }
+  ngOnInit(): void {
+  }
 
 }
--- src/app/components/admin/cars/cars.component.ts
+++ src/app/components/admin/cars/cars.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ICar} from "../../../interfaces";
+import {CarService} from "../../../services";
 
 @Component({
   selector: 'app-cars',
   templateUrl: './cars.component.html',
   styleUrls: ['./cars.component.css']
 })
-export class CarsComponent {
+export class CarsComponent implements OnInit{
+  cars:ICar[]
+  constructor(private carService : CarService) {
+  }
+  ngOnInit(): void {
+    this.carService.getAll().subscribe(value => this.cars = value)
+  }
 
 }
